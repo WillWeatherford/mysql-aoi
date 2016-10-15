b82d18ce1faf6eecbe417dfe7f882d8d5bbde6c3
Will Weatherford <weatherford.william@gmail.com>
2016-10-15 13:58:32 -0700

    Added example config file.

---
+++ example_config.py
@@ -0,0 +1,22 @@
+"""Local config for MySQL connection."""
+
+HOST = '00.00.00.00'
+USER = 'username'
+PASSWORD = 'password'
+DATABASE = 'dbname'
+
+DEFAULT_CONFIG = {
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
+    'database': DATABASE,
+}
+
+VALID_TABLES = {'table1', 'table2', 'table3'}
+VALID_COLUMN_NAMES = [
+    'col1',
+    'col2',
+    'table1_col1',
+    'table2_col2',
+]
+COLS = dict(zip(VALID_COLUMN_NAMES, VALID_COLUMN_NAMES))
