2516c68ef4be497552948c9bc1293c6f4036baf0
Will Weatherford <weatherford.william@gmail.com>
2016-10-15 15:44:47 -0700

    Basic test fixture to create tables.

--- test_mysql_api.py
+++ test_mysql_api.py
@@ -1,3 +1,16 @@
 """Test module for MySQL API."""
 
 import pytest
+import test_config
+from mysql import connector
+
+
+@pytest.fixture
+def create_tables():
+    """Create tables for testing."""
+    conn = connector.connect(**test_config.TEST_CONFIG)
+    cursor = conn.cursor()
+    cursor.execute(test_config.TABLES['company'])
+    cursor.close()
+    conn.close()
+
